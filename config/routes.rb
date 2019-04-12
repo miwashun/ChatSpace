8ff85baba662e711586554a39b6ed48255ef8772
miwashun <kals_miwashun123@yahoo.co.jp>
2019-04-12 19:03:37 +0900

    Routing configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: %i[edit update]
-  resources :groups, only: %i[new create edit update] do
+  resources :groups, only: %i[index new create edit update] do
   end
 end
