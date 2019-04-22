a156f37573dc9b37e4052892d7564a69d14939d6
miwashun <kals_miwashun123@yahoo.co.jp>
2019-04-22 12:15:52 +0900

    Create action test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -34,6 +34,57 @@ describe MessagesController do
         expect(response).to redirect_to(new_user_session_path)
       end
     end
+  end
 
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(message) } }
+
+    context 'log in(ログインしている場合のテスト)' do
+      before do
+        login user
+      end
+
+      context 'can save(メッセージの保存に成功した場合)' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message(メッセージの保存はできたのか)' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path(意図した画面に遷移しているか)' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context 'can not save(メッセージの保存に失敗した場合)' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, context: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up(メッセージの保存は行われなかったか)' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'renders index(意図したビューが描画されているか)' do
+          subject
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+    end
+
+    context 'not log in(ログインしていない場合)' do
+
+      it 'redirects to new_user_session_path(意図した画面にリダイレクトできているか)' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
 end
