9cbe22417e960282897da0b6c4a54aef9d7044f2
miwashun <kals_miwashun123@yahoo.co.jp>
2019-04-26 19:26:05 +0900

    Message storage function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,13 @@ $(function(){
   $('#create_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+    $.ajax({
+      url: location.href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
