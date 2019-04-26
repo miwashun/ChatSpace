275b52dd8be2b51509e01aed5d58d602e1e998f1
miwashun <kals_miwashun123@yahoo.co.jp>
2019-04-26 19:52:52 +0900

    Create HTML
    Receive the returned JSON with the done method and create HTML.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,31 @@
 $(function(){
+
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__user">
+                      <%= message.user.name %>
+                    </p>
+                    <p class="message__upper-info__date">
+                      <%= message.created_at.strftime("%Y/%m/%d %H:%M")  %>
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    <% if message.content.present? %>
+                      <p class="lower-message__content">
+                        <%= message.content %>
+                      </p>
+                    <% end %>
+                    <%= image_tag message.image.url, class: 'form__mask__image' if message.image.present? %>
+                  </p>
+                </div>`
+    return html;
+  }
+
   $('#create_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+
     $.ajax({
       url: location.href,
       type: "POST",
@@ -10,5 +34,11 @@ $(function(){
       processData: false,
       contentType: false
     })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      messages.append(html)
+    })
+
   })
 })
