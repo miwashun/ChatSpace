de8a80bd9b9d4dbaab7eede130d09bbfaba5a884
miwashun <kals_miwashun123@yahoo.co.jp>
2019-04-28 12:46:41 +0900

    Additional routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: %i[edit update]
+  resources :users, only: %i[index edit update]
   resources :groups, only: %i[ new create edit update] do
     resources :messages, only: %i[index create]
   end
