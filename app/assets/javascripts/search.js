c62bcd6b72f30a7ef0854ed6f7399b5cb06d103a
miwashun <kals_miwashun123@yahoo.co.jp>
2019-04-28 20:27:11 +0900

    Make error handling

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,5 @@
 $(function() {
-  function appendUser(user) {
+  function appendSearchResult(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -10,21 +10,32 @@ $(function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-
-    .done(function(users) {
-      users.forEach(function(user){
-        appendUser(user);
-        console.log(user);
+    if (input != "") {
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
       })
-    })
 
-  })
-})
+      .done(function(users) {
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendSearchResult(user);
+          })
+        }
+      })
+
+      .fail(function() {
+        alert('名前検索に失敗しました。');
+      })
+    }
+
+    else {
+      $("#user-search-result").empty();
+    }
+
+  });
+});
 
