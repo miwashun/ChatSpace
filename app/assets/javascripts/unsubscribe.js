de309d7efb0d20c78da928e4e8c58248ce2b27fb
miwashun <kals_miwashun123@yahoo.co.jp>
2019-04-29 12:46:29 +0900

    Add html

--- app/assets/javascripts/unsubscribe.js
+++ app/assets/javascripts/unsubscribe.js
@@ -1,5 +1,13 @@
 
 $(function() {
+  function addbtn(user_id,user_name) {
+    var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-${user_id}">
+                  <input value="${user_id}" name="group[user_ids][]" type="hidden" id="group_user_ids">
+                  <p class="chat-group-user__name">${user_name}</p>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
+                </div>`
+                $(".chat-group-users.js-add-user").append(html);
+  }
   $("#user-search-result").on('click',".chat-group-user__btn--add",function(){
     var id = $(this).attr('data-user-id');
     var name = $(this).attr('data-user-name');
