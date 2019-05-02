25910c7eccf99715f619fd0816445fc2ca6e2ff0
miwashun <kals_miwashun123@yahoo.co.jp>
2019-05-02 20:10:37 +0900

    Add

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,8 @@
 class Api::MessagesController < ApplicationController
   def index
+    respond_to do |format|
+      format.html
+      format.json { @new_message = @messages.where'id > ?', params[:id] }
+    end
   end
 end
