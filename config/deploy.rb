37480bd2b49122b3757e970a7cb6bb9c60b00b86
miwashun <kals_miwashun123@yahoo.co.jp>
2019-05-06 12:45:37 +0900

    Modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,9 @@ set :repo_url, "git@github.com:miwashun/ChatSpace.git"
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby_version, 'ruby_2.3.1'
+set :rbenv_custom_path, '/root/.rbenv'
+
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
